29d7041622582302ea5f016a271ebd725b155577
berhart <bernasrsa@gmail.com>
2024-05-22 22:29:59 +0100

    minor changes

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,18 @@
 import { resolveResource } from '@tauri-apps/api/path';
 import { readTextFile } from '@tauri-apps/api/fs';
 
-import {
-	PUBLIC_DEBUG,
-} from '$env/static/public';
+import { PUBLIC_DEBUG } from '$env/static/public';
 
 let initialConfig: Record<string, string> = {};
 
 try {
-	const resourcePath = await resolveResource('config.json');
-	initialConfig = JSON.parse(await readTextFile(resourcePath));
+	if (window.__TAURI__) {
+		const resourcePath = await resolveResource('config.json');
+		initialConfig = JSON.parse(await readTextFile(resourcePath));
+	} else {
+		const response = await fetch('/config.json');
+		initialConfig = await response.json();
+	}
 } catch (e) {
 	console.error('Error fetching config.json', e);
 }
@@ -19,7 +22,7 @@ console.log(initialConfig);
 const { debug } = initialConfig;
 
 const config = {
-	debug: debug ?? PUBLIC_DEBUG,
+	debug: debug ?? PUBLIC_DEBUG
 };
 
 export default config;
